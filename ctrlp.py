975c7d08fadcbaa9f3cc9c6f59b8468c731b5aac
Jamshid Ahmadov <117648241+jamshid-ds@users.noreply.github.com>
2023-05-05 07:59:40 +0500

    Add files via upload

---
+++ ctrlp.py
@@ -0,0 +1,24 @@
+list1 = list(map(str,input().split(",")))
+
+list2 = []
+for t in list1:
+    if t=="-":
+        for y in range(1,2625):
+            list2.append(y)
+    elif "-" in t and len(t)>1:
+        if t.index("-")==0:
+            b = t[t.index("-")+1::]
+            for y in range(1,int(b)+1):
+                list2.append(y)
+        elif t.index("-")==len(t)-1:
+            b = t[:t.index("-")]
+            for x in range(int(b),2625):
+                list2.append(x)
+        else:
+        
+            a,c = t.split("-")
+            for y in range(int(a),int(c)+1):
+                    list2.append(y)
+    else:
+        list2.append(t)
+print(len(list2))
